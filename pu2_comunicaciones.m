3be6536fdfce97cea15b388c3c22d1cfc86018eb
jerogt <jeronimogomeztantarelli@yahoo.com.ar>
2023-07-27 14:54:07 -0300

    Arreglo del script (cambio en fft_kit)

--- pu2_comunicaciones.m
+++ pu2_comunicaciones.m
@@ -11,8 +11,8 @@ Ts=1/fs;
 n=0:length(x)-1;
 
 %% ESTUDIO DEL ESPECTRO
-[X,X_shift,f] = fft_kit(x,fs);
-stemCompleto([-fs/2 fs/2 0 abs(max(X))+10], 'f [KHz]', 'Amplitud', 'Espectro de la señal de audio', 20, 'r.', 0.5, f, abs(X_shift));
+[X_s,f] = fft_kit(x,fs);
+stemCompleto([-fs/2 fs/2 0 abs(max(X_s))+10], 'f [KHz]', 'Amplitud', 'Espectro de la señal de audio', 20, 'r.', 0.5, f, abs(X_s));
 xticks([-22050, -16000, -8000, 0, 8000, 16000, 22050]);
 xticklabels({'-22.05', '-16', '-8', '0', '8', '16', '22.05'})
 
@@ -22,8 +22,8 @@ load('./filtros/filtroBP_4k-12k.mat');
 y1=filter(BP,x);
 stemCompleto([0 n(end) -1  1], 'n', 'Amplitud', 'y1[n]', 20, 'b.',0.5, n, y1);
 
-[X_BP,X_BP_shift]=fft_kit(y1, fs);
-stemCompleto([-fs/2 fs/2 0 abs(max(X_BP))+10], 'f [KHz]', 'Amplitud', 'Espectro filtrado (pasabanda)', 20, 'r.', 0.5, f, abs(X_BP_shift));
+X_BP_s = fft_kit(y1, fs);
+stemCompleto([-fs/2 fs/2 0 abs(max(X_BP_s))+10], 'f [KHz]', 'Amplitud', 'Espectro filtrado (pasabanda)', 20, 'r.', 0.5, f, abs(X_BP_s));
 xticks([-22050, -16000, -8000, 0, 8000, 16000, 22050]);
 xticklabels({'-22.05', '-16', '-8', '0', '8', '16', '22.05'})
 
@@ -33,8 +33,8 @@ yd=(cos(2*pi*fp1*n*Ts + theta))';
 z1=y1.*yd;
 stemCompleto([0 n(end) -1  1], 'n', 'Amplitud', 'z1[n]', 20, 'b.',0.5, n, z1);
 
-[Z1,Z1_shift,~]=fft_kit(z1,fs);
-stemCompleto([-fs/2 fs/2 0 (max(abs(Z1))*1.1)], 'f [KHz]', 'Amplitud', '|Z1[k]|', 20, 'r.', 0.5, f, abs(Z1_shift));
+Z1_s = fft_kit(z1,fs);
+stemCompleto([-fs/2 fs/2 0 (max(abs(Z1_s))*1.1)], 'f [KHz]', 'Amplitud', '|Z1[k]|', 20, 'r.', 0.5, f, abs(Z1_s));
 xticks([-22050, -16000, -8000, 0, 8000, 16000, 22050]);
 xticklabels({'-22.05', '-16', '-8', '0', '8', '16', '22.05'})
 
@@ -42,6 +42,5 @@ xticklabels({'-22.05', '-16', '-8', '0', '8', '16', '22.05'})
 % save('./filtros/filtroLP_8k.mat','LP');
 load('./filtros/filtroLP_8k.mat');
 x1=filter(LP,z1);
-sound(x1,fs);
 
 
