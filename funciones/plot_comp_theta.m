1cb082a2bf4afb8b3a06d98443e27741b55d3a77
Ignacio Brittez <ignacio_brittez@outlook.com>
2023-07-28 11:21:38 -0300

    Codigo comp theta retorno a su estado anterior (etiquetas en 22.05k)

--- funciones/plot_comp_theta.m
+++ funciones/plot_comp_theta.m
@@ -23,10 +23,8 @@ function plot_comp_theta(opcion,titulo,vector, fp, n, Ts, y)
             [Z_s, f] = fft_kit(z,1/Ts);
             plot(f, abs(Z_s));
             axis([-1/(2*Ts) 1/(2*Ts) 0 (max(abs(Z_s))*1.1)]);
-            xticks([-16000, -8000, 0, 8000, 16000]);
-            % xticks([-22050, -16000, -8000, 0, 8000, 16000, 22050]);
-            %xticklabels({'-22.05', '-16', '-8', '0', '8', '16', '22.05'})
-            xticklabels({'-16', '-8', '0', '8', '16'})
+            xticks([-22050, -16000, -8000, 0, 8000, 16000, 22050]);
+            xticklabels({'-22.05', '-16', '-8', '0', '8', '16', '22.05'})
             grid on;
             set(gca,'FontSize', 12); % Tama~no de letra para la leyenda y ejes.
             title("\alpha = (" + vector(i)/pi + ")\cdot\pi", 'FontWeight', 'normal');
